ca11f6bad8b066190140b984922f375a8fff1a69
goodboy95 <501217662@qq.com>
2016-10-24 00:32:13 +0800

    Update:some bug fixes
    (1)No error messages when creating a second account in one computer.
    (2)A warning message will be shown while inputting a out_of_bound number in the account_choosing menu.

--- yah3c/yah3c.py
+++ yah3c/yah3c.py
@@ -70,33 +70,38 @@ def enter_interactive_usermanager():
     if um.get_user_number() == 0:
         choice = raw_input('No user conf file found, creat a new one?\n<Y/N>: ')
         if choice == 'y' or choice == 'Y': 
-            login_info = prompt_user_info()
-            um.add_user(login_info)
+            user_info = prompt_user_info()
+            um.add_user(user_info)
         else: 
             exit(-1)
     
     # user has been created or already have users
-    users_info = um.get_all_users_info()
-
-    print '0 - add a new user'
-    for i, user_info in enumerate(users_info):
-        print '%d - %s(%s)' %(i + 1, user_info['username'], user_info['ethernet_interface'])
-
     while True:
+        users_info = um.get_all_users_info()
+        print 'Press Ctrl+C to exit'
+        print '0 - add a new user'
+        for i, user_info in enumerate(users_info):
+            print '%d - %s(%s)' %(i + 1, user_info['username'], user_info['ethernet_interface'])
+
         try:
             choice = int(raw_input('Your choice: '))
-        except ValueError:
-            print 'Please input a valid number!'
-        else: break;
-    if choice == 0:
-        try:
-            user_info = prompt_user_info()
-            um.add_user(user_info)
-        except ConfigParser.DuplicateSectionError:
-            print 'User already exist!'
-            exit(-1)
-    else: 
-        return users_info[choice - 1]
+        except ValueError,e:
+            print '\nPlease input a valid number!\n'
+            continue
+        if choice == 0:
+            try:
+                user_info = prompt_user_info()
+                um.add_user(user_info)
+                continue
+            except ConfigParser.DuplicateSectionError:
+                print '\nUser already exist!\n'
+                continue
+        elif choice <= len(users_info): 
+            return users_info[choice - 1]
+        else:
+            print '\nPlease input a valid number!\n'
+            continue
+        
 
 def start_yah3c(login_info):
     yah3c = eapauth.EAPAuth(login_info)
